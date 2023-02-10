6a2c9f1da2892be00ab97e5a5adf41bab452a8df
Your Name <you@example.com>
2023-02-10 11:46:43 +0100

    prices route

--- controllers/gym.js
+++ controllers/gym.js
@@ -19,3 +19,22 @@ export const Opening=(request,response)=>{
         response.send(result)
     })
 }
+export const Prices =(request,response)=>{
+    console.log(request.body)
+    db.query('SELECT  *  from prices order by id',(err,result)=>{
+        if(err)
+            console.log('HIBA!',err)
+        else
+        response.send(result)
+    })
+}
+
+export const Category =(request,response)=>{
+    console.log(request.body)
+    db.query('SELECT c.id, p.Egeszar,p.kedvezmenyesar,p.megjegyzes,c.description from prices p, category c WHERE p.categid = c.id',(err,result)=>{
+        if(err)
+            console.log('HIBA!',err)
+        else
+        response.send(result)
+    })
+}
--- routes/gymRouter.js
+++ routes/gymRouter.js
@@ -1,6 +1,8 @@
 import express from "express";
-import {Opening} from "../controllers/gym.js"
+import {Category, Opening, Prices} from "../controllers/gym.js"
 
 export const gymRouter=express.Router();
 
 gymRouter.route('/opening').get(Opening);
+gymRouter.route('/prices').get(Prices);
+gymRouter.route('/category').get(Category);
